5c6317227fb429b55591ca408c23cf70157f1659
Atiqur Rahman <39456914+atiq275217@users.noreply.github.com>
2019-11-13 13:17:29 +0600

    Add files via upload

---
+++ w3main.js
@@ -0,0 +1,9 @@
+// innerHTML
+document.getElementById("inner_HTML").innerHTML = 5 + 6;
+
+
+document.getElementById("demo_1").innerHTML = "My First Practece JS";
+function myFunction_2() {
+	document.getElementById("demo_2").innerHTML = "Paragraph Change Practece JS";
+
+}
