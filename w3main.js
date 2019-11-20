db2aa8260c6292125544fecf71fc93e6f7327841
Atiqur Rahman <39456914+atiq275217@users.noreply.github.com>
2019-11-20 13:02:30 +0600

    Delete w3main.js

--- w3main.js
+++
@@ -1,9 +0,0 @@
-// innerHTML
-document.getElementById("inner_HTML").innerHTML = 5 + 6;
-
-
-document.getElementById("demo_1").innerHTML = "My First Practece JS";
-function myFunction_2() {
-	document.getElementById("demo_2").innerHTML = "Paragraph Change Practece JS";
-
-}
